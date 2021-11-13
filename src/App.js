1afaadb971c459042e90315067798d94f70c3243
kwangmin-park <96bgm@korea.ac.kr>
2021-11-13 21:30:37 +0900

    category 작업 중

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import './App.css';
 import axios from "axios";
 import React, {useState} from "react";
+import NewsList from "./componenets/NewsList";
+import Categories from "./componenets/Categories";
 
 function App() {
-    const [data, setData] = useState(null);
-    const onClick = async () => {
-        const response = await axios.get("https://newsapi.org/v2/top-headlines?country=kr&apiKey=1ea0620b6014469dbafcbd8f58fa02ea");
-            setData(response.data);
-        try {
-        } catch(e){
-            console.log(e);
-        }
-    }
     return (
-        <div>
-            <div>
-                <button onClick={onClick}>불러오기</button>
-            </div>
-            {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}/>}
-        </div>
+        <>
+            <Categories/>
+            <NewsList/>
+        </>
     );
 }
 
