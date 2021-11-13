336ff22b206968466b5b6e5bc35e797e2086a85f
kwangmin-park <96bgm@korea.ac.kr>
2021-11-13 22:43:48 +0900

    usePromise customcallback function 사용

--- src/componenets/NewsList.js
+++ src/componenets/NewsList.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
 import axios from "axios";
+import usePromise from "../lib/usePromise";
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -18,32 +19,24 @@ const NewsListBlock = styled.div`
 
 
 const NewsList = ({category}) => {
-    const [articles, setArticles] = useState(null);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(()=>{
-        const fetchData = async () => {
-            setLoading(true);
-            try{
-                const query = category === 'all' ? '' : `&category=${category}`;
-                const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=1ea0620b6014469dbafcbd8f58fa02ea`);
-                setArticles(response.data.articles);
-            }catch (e){
-                console.log(e);
-            }
-            setLoading(false);
-        };
-        fetchData();
+    const [loading, response, error] = usePromise(() => {
+        const query = category === 'all' ? '' : `&category=${category}`;
+        return axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=1ea0620b6014469dbafcbd8f58fa02ea`);
     }, [category]);
 
     if(loading){
         return <NewsListBlock>대기중 ...</NewsListBlock>
     }
 
-    if(!articles){
+    if(!response){
         return null;
     }
 
+    if(error){
+        return <NewsListBlock>에러 발생!</NewsListBlock>
+    }
+
+    const {articles} = response.data;
     return (
         <NewsListBlock>
             {articles.map(article => (
